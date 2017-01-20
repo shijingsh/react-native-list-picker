e53a48d479fc18498b16630d454dbcc18cee3e1e
刘克付 <liukefu2020@sina.com>
2017-01-20 14:56:38 +0800

    example

--- src/ListPicker.js
+++ src/ListPicker.js
@@ -3,12 +3,12 @@
  * @author liukefu
  */
 
-import React, { Component } from 'react';
-import { View, Image, TouchableOpacity, Modal, Text, ListView, Platform } from 'react-native';
+import React, {Component} from 'react';
+import {View, Image, TouchableOpacity, Modal, Text, ListView, Platform} from 'react-native';
 import _ from 'lodash';
 
 import cca2List from '../data/cca2';
-import { getHeightPercent } from './ratio';
+import {getHeightPercent} from './ratio';
 import CloseButton from './CloseButton';
 import styles from './Picker.style';
 
@@ -22,195 +22,195 @@ let Emoji = null;
 const isEmojiable = Platform.OS === 'ios';
 
 if (isEmojiable) {
-  countries = require('../data/countries-emoji');
-  Emoji = require('react-native-emoji').default;
+    countries = require('../data/countries-emoji');
+    Emoji = require('react-native-emoji').default;
 } else {
-  countries = require('../data/countries');
+    countries = require('../data/countries');
 
-  Emoji = <View />;
+    Emoji = <View />;
 }
 
-const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
+const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
 
 export default class ListPicker extends Component {
-  static propTypes = {
-    cca2: React.PropTypes.string.isRequired,
-    translation: React.PropTypes.string,
-    onChange: React.PropTypes.func.isRequired,
-    closeable: React.PropTypes.bool,
-    children: React.PropTypes.node,
-  }
-  static defaultProps = {
-    translation: 'eng',
-  }
-
-  state = {
-    modalVisible: false,
-    cca2List,
-    dataSource: ds.cloneWithRows(cca2List),
-  };
-
-  onSelectCountry(cca2) {
-    this.setState({
-      modalVisible: false,
-    });
-
-    this.props.onChange({
-      cca2,
-      ...countries[cca2],
-      flag: undefined,
-      name: this.getCountryName(countries[cca2]),
-    });
-  }
-
-  getCountryName(country, optionalTranslation) {
-    const translation = optionalTranslation || this.props.translation || 'eng';
-    return country.name[translation] || country.name.common;
-  }
-
-  setVisibleListHeight(offset) {
-    this.visibleListHeight = getHeightPercent(100) - offset;
-  }
-
-  openModal = this.openModal.bind(this);
-  letters = _
-    .range('A'.charCodeAt(0), 'Z'.charCodeAt(0) + 1)
-    .map(n => String.fromCharCode(n).substr(0));
-
-  // dimensions of country list and window
-  itemHeight = getHeightPercent(7);
-  listHeight = countries.length * this.itemHeight;
-
-  openModal() {
-    this.setState({ modalVisible: true });
-  }
-
-  scrollTo(letter) {
-    // find position of first country that starts with letter
-    const index = this.state.cca2List.map((country) => countries[country].name.common[0])
-      .indexOf(letter);
-    if (index === -1) {
-      return;
+    static propTypes = {
+        cca2: React.PropTypes.string.isRequired,
+        translation: React.PropTypes.string,
+        onChange: React.PropTypes.func.isRequired,
+        closeable: React.PropTypes.bool,
+        children: React.PropTypes.node,
     }
-    let position = index * this.itemHeight;
-
-    // do not scroll past the end of the list
-    if (position + this.visibleListHeight > this.listHeight) {
-      position = this.listHeight - this.visibleListHeight;
+    static defaultProps = {
+        translation: 'eng',
     }
 
-    // scroll
-    this._listView.scrollTo({
-      y: position,
-    });
-  }
+    state = {
+        modalVisible: false,
+        cca2List,
+        dataSource: ds.cloneWithRows(cca2List),
+    };
 
-  renderCountry(country, index) {
-    return (
-      <TouchableOpacity
-        key={index}
-        onPress={() => this.onSelectCountry(country)}
-        activeOpacity={0.99}
-      >
-        {this.renderCountryDetail(country)}
-      </TouchableOpacity>
-    );
-  }
+    onSelect(cca2) {
+        this.setState({
+            modalVisible: false,
+        });
 
-  renderLetters(letter, index) {
-    return (
-      <TouchableOpacity
-        key={index}
-        onPress={() => this.scrollTo(letter)}
-        activeOpacity={0.6}
-      >
-        <View style={styles.letter}>
-          <Text style={styles.letterText}>{letter}</Text>
-        </View>
-      </TouchableOpacity>
-    );
-  }
+        this.props.onChange({
+            cca2,
+            ...countries[cca2],
+            flag: undefined,
+            name: this.getCountryName(countries[cca2]),
+        });
+    }
 
-  renderCountryDetail(cca2) {
-    const country = countries[cca2];
-    return (
-      <View style={styles.itemCountry}>
-        {ListPicker.renderFlag(cca2)}
-        <View style={styles.itemCountryName}>
-          <Text style={styles.countryName}>
-            {this.getCountryName(country)}
-          </Text>
-        </View>
-      </View>
-    );
-  }
+    getCountryName(country, optionalTranslation) {
+        const translation = optionalTranslation || this.props.translation || 'eng';
+        return country.name[translation] || country.name.common;
+    }
 
-  static renderEmojiFlag(cca2, emojiStyle) {
-    return (
-      <Text style={[ styles.emojiFlag, emojiStyle ]}>
-        <Emoji name={countries[cca2].flag} />
-      </Text>
-    );
-  }
+    setVisibleListHeight(offset) {
+        this.visibleListHeight = getHeightPercent(100) - offset;
+    }
 
-  static renderImageFlag(cca2, imageStyle) {
-    return (
-      <Image
-        style={[ styles.imgStyle, imageStyle ]}
-        source={{ uri: countries[cca2].flag }}
-      />
-    );
-  }
+    openModal = this.openModal.bind(this);
+    letters = _
+        .range('A'.charCodeAt(0), 'Z'.charCodeAt(0) + 1)
+        .map(n => String.fromCharCode(n).substr(0));
 
-  static renderFlag(cca2, itemStyle, emojiStyle, imageStyle) {
-    return (
-      <View style={[ styles.itemCountryFlag, itemStyle ]}>
-        {isEmojiable ? ListPicker.renderEmojiFlag(cca2, emojiStyle) : ListPicker.renderImageFlag(cca2, imageStyle)}
-      </View>
-    );
-  }
+    // dimensions of country list and window
+    itemHeight = getHeightPercent(7);
+    listHeight = countries.length * this.itemHeight;
 
-  render() {
-    return (
-      <View>
-        <TouchableOpacity
-          onPress={() => this.setState({ modalVisible: true })}
-          activeOpacity={0.7}>
-          {
-            this.props.children ?
-              this.props.children
-            :
-              (<View style={styles.touchFlag}>
-                {ListPicker.renderFlag(this.props.cca2)}
-              </View>)
-          }
-        </TouchableOpacity>
-        <Modal
-          visible={this.state.modalVisible}
-          onRequestClose={() => this.setState({ modalVisible: false })}
-        >
-          <View style={styles.modalContainer}>
-            {
-              this.props.closeable &&
-                <CloseButton onPress={() => this.setState({ modalVisible: false })} />
-            }
-            <ListView
-              contentContainerStyle={styles.contentContainer}
-              ref={listView => this._listView = listView}
-              dataSource={this.state.dataSource}
-              renderRow={country => this.renderCountry(country)}
-              initialListSize={30}
-              pageSize={countries.length - 30}
-              onLayout={
-                ({ nativeEvent: { layout: { y: offset } } }) => this.setVisibleListHeight(offset)
-              }
-            />
-            <View style={styles.letters}>
-              {this.letters.map((letter, index) => this.renderLetters(letter, index))}
+    openModal() {
+        this.setState({modalVisible: true});
+    }
+
+    scrollTo(letter) {
+        // find position of first country that starts with letter
+        const index = this.state.cca2List.map((country) => countries[country].name.common[0])
+            .indexOf(letter);
+        if (index === -1) {
+            return;
+        }
+        let position = index * this.itemHeight;
+
+        // do not scroll past the end of the list
+        if (position + this.visibleListHeight > this.listHeight) {
+            position = this.listHeight - this.visibleListHeight;
+        }
+
+        // scroll
+        this._listView.scrollTo({
+            y: position,
+        });
+    }
+
+    renderRow(row, index) {
+        return (
+            <TouchableOpacity
+                key={index}
+                onPress={() => this.onSelect(row)}
+                activeOpacity={0.99}
+            >
+                {this.renderDetail(row)}
+            </TouchableOpacity>
+        );
+    }
+
+    renderLetters(letter, index) {
+        return (
+            <TouchableOpacity
+                key={index}
+                onPress={() => this.scrollTo(letter)}
+                activeOpacity={0.6}
+            >
+                <View style={styles.letter}>
+                    <Text style={styles.letterText}>{letter}</Text>
+                </View>
+            </TouchableOpacity>
+        );
+    }
+
+    renderDetail(cca2) {
+        const country = countries[cca2];
+        return (
+            <View style={styles.itemCountry}>
+                {ListPicker.renderFlag(cca2)}
+                <View style={styles.itemCountryName}>
+                    <Text style={styles.countryName}>
+                        {this.getCountryName(country)}
+                    </Text>
+                </View>
             </View>
-          </View>
-        </Modal>
-      </View>
-    );
-  }
+        );
+    }
+
+    static renderEmojiFlag(cca2, emojiStyle) {
+        return (
+            <Text style={[styles.emojiFlag, emojiStyle]}>
+                <Emoji name={countries[cca2].flag}/>
+            </Text>
+        );
+    }
+
+    static renderImageFlag(cca2, imageStyle) {
+        return (
+            <Image
+                style={[styles.imgStyle, imageStyle]}
+                source={{uri: countries[cca2].flag}}
+            />
+        );
+    }
+
+    static renderFlag(cca2, itemStyle, emojiStyle, imageStyle) {
+        return (
+            <View style={[styles.itemCountryFlag, itemStyle]}>
+                {isEmojiable ? ListPicker.renderEmojiFlag(cca2, emojiStyle) : ListPicker.renderImageFlag(cca2, imageStyle)}
+            </View>
+        );
+    }
+
+    render() {
+        return (
+            <View>
+                <TouchableOpacity
+                    onPress={() => this.setState({modalVisible: true})}
+                    activeOpacity={0.7}>
+                    {
+                        this.props.children ?
+                            this.props.children
+                            :
+                            (<View style={styles.touchFlag}>
+                                {ListPicker.renderFlag(this.props.cca2)}
+                            </View>)
+                    }
+                </TouchableOpacity>
+                <Modal
+                    visible={this.state.modalVisible}
+                    onRequestClose={() => this.setState({modalVisible: false})}
+                >
+                    <View style={styles.modalContainer}>
+                        {
+                            this.props.closeable &&
+                            <CloseButton onPress={() => this.setState({modalVisible: false})}/>
+                        }
+                        <ListView
+                            contentContainerStyle={styles.contentContainer}
+                            ref={listView => this._listView = listView}
+                            dataSource={this.state.dataSource}
+                            renderRow={row => this.renderRow(row)}
+                            initialListSize={30}
+                            pageSize={countries.length - 30}
+                            onLayout={
+                                ({nativeEvent: {layout: {y: offset}}}) => this.setVisibleListHeight(offset)
+                            }
+                        />
+                        <View style={styles.letters}>
+                            {this.letters.map((letter, index) => this.renderLetters(letter, index))}
+                        </View>
+                    </View>
+                </Modal>
+            </View>
+        );
+    }
 }
